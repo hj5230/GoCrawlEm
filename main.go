d79b91008671c5c6a709e81d70cf19266f1d264a
Shangqi Z <98270829+hj5230@users.noreply.github.com>
2024-06-02 09:58:51 +0000

    test: remove xinhuaresearch duplicates

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/hj5230/GoCrawlEm/gocrawlem/cndaily"
+	csvutils "github.com/hj5230/GoCrawlEm/csv_utils"
 )
 
 func main() {
-	cndaily.CrawlFromJson()
-	// cndaily.CrawlPostContent("https://regional.chinadaily.com.cn/ordosen/en/2023-12/19/c_949003.htm")
+	csvutils.RemoveDuplicates()
 }
