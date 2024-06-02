263a18dd90bc8914e80128e87f87aca93421843f
Shangqi Z <98270829+hj5230@users.noreply.github.com>
2024-06-02 07:30:58 +0000

    test: crawl chinadaily from url json

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 )
 
 func main() {
-	// xinhuar.Crawl()
-	// title, info, content := cndaily.CrawlPostContent("https://www.chinadaily.com.cn/a/202403/07/WS65e928cba31082fc043bb29e.html")
-	// fmt.Println(title, info)
-	// fmt.Println(content)
 	cndaily.CrawlFromJson()
+	// cndaily.CrawlPostContent("https://regional.chinadaily.com.cn/ordosen/en/2023-12/19/c_949003.htm")
 }
