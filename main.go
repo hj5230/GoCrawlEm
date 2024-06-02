4ba3ab5bb632fa600549b82c4a5d3f443572596e
Shangqi Z <98270829+hj5230@users.noreply.github.com>
2024-06-02 06:18:42 +0000

    test: crawl chinadaily from urls json

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"github.com/hj5230/GoCrawlEm/gocrawlem/xinhuar"
+	"github.com/hj5230/GoCrawlEm/gocrawlem/cndaily"
 )
 
 func main() {
-	xinhuar.Crawl()
+	// xinhuar.Crawl()
+	// title, info, content := cndaily.CrawlPostContent("https://www.chinadaily.com.cn/a/202403/07/WS65e928cba31082fc043bb29e.html")
+	// fmt.Println(title, info)
+	// fmt.Println(content)
+	cndaily.CrawlFromJson()
 }
